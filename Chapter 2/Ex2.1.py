c6cc05a50321011ab05d21185839533b18cdf052
datooff24 <132133001+datooff24@users.noreply.github.com>
2023-04-29 12:02:34 +0200

    Create Ex2.1.py

---
+++ Chapter 2/Ex2.1.py
@@ -0,0 +1,12 @@
+all = "All"
+work = " work"
+_and_ = " and"
+no = " no"
+play = " play"
+makes = " makes"
+jack = " Jack"
+a = " a"
+dull = " dull"
+boy = " boy."
+
+print(all+work+_and_+no+play+makes+jack+a+dull+boy)
