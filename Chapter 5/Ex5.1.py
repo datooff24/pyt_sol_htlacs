e433b77a8740108f5215e5156224406e71d96512
datooff24 <132133001+datooff24@users.noreply.github.com>
2023-04-29 12:07:24 +0200

    Create Ex5.1.py

---
+++ Chapter 5/Ex5.1.py
@@ -0,0 +1,19 @@
+def number_to_day(num):
+    if num == 0:
+        print("Sunday")
+    elif num == 1:
+        print("Monday")
+    elif num == 2:
+        print("Tuesday")
+    elif num == 3:
+        print("Wednesday")
+    elif num == 4:
+        print("Thursday")
+    elif num == 5:
+        print("Friday")
+    elif num == 6:
+        print("Saturday")
+    else:
+        print("Please enter a number from 0 to 6")
+
+number_to_day(1)
