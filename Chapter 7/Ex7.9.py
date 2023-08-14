f13672177192b6e747b9f36b8b2ad19b357533f3
datooff24 <132133001+datooff24@users.noreply.github.com>
2023-08-14 18:39:54 +0200

    Update Ex7.9.py

--- Chapter 7/Ex7.9.py
+++ Chapter 7/Ex7.9.py
@@ -6,3 +6,16 @@ def print_triangular_numbers(n):
 
 
 print_triangular_numbers(5)
+
+# ------Alt solution-------
+
+def print_triangular_numbers(n):
+    num = 1
+    sum = num
+    for i in range(n):
+        print(num, end="       ")
+        print(sum)
+        num += 1
+        sum += num
+
+print_triangular_numbers(5)
