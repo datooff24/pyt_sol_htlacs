dd62207f3478d34d44f893161abde4b1c5bb94db
datooff24 <132133001+datooff24@users.noreply.github.com>
2023-08-15 14:31:32 +0200

    Update Ex8.4.py

--- Chapter 8/Ex8.4.py
+++ Chapter 8/Ex8.4.py
@@ -10,3 +10,28 @@ def count_letters(string, letter, start=0):
 
 
 print(count_letters("banana", "a"))
+
+
+#-----Alt solution-----
+# Idea:
+# find method gives the position of the FIRST occurrence of the letter
+# call find method to find the first occurrence
+# call find method again, starting one spot after the position of the found letter
+# repeat until the start position is too large to be possible to be used
+
+
+def count_letters(strng, letter, start=0):
+    count = 0
+    while start < len(strng):
+        position = strng.find(letter, start)
+        print(position)
+        if position == -1:
+            return count
+        else:
+            count += 1
+            start = position+1
+            print("start=", start)
+    return count
+
+
+print(count_letters("banana", "a"))
