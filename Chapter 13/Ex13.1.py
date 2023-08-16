59bef1924bd8843d96df620d4bcb7d3821090da6
datooff24 <132133001+datooff24@users.noreply.github.com>
2023-08-16 19:34:51 +0200

    Update Ex13.1.py

--- Chapter 13/Ex13.1.py
+++ Chapter 13/Ex13.1.py
@@ -1,11 +1,13 @@
-def reverse_file(original, reverse):
-    old = open(original, "r")
-    xs = old.readlines()
-    old.close()
+def reverse_files(original, reversed="reversed"):
+    """copies the content of the file original and pastes them in a new file reversed"""
+    # separate each line of original one list and reverse them
+    old_file = open(original, "r")
+    line_list = old_file.readlines()
+    line_list.reverse()
+    old_file.close()
 
-    xs.reverse()
-
-    new = open(reverse, "w")
-    for word in xs:
-        new.write(word)
-    new.close()
+    # open a new file, and put each line of the reverse list in there
+    new_file = open(reversed, "w")
+    for line in line_list:
+        new_file.write(line)
+    new_file.close()
