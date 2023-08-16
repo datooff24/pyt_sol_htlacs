56aaaf303a792217d50a48c7657c52c45aaf3dea
datooff24 <132133001+datooff24@users.noreply.github.com>
2023-08-16 20:29:24 +0200

    Update Ex13.4.py

--- Chapter 13/Ex13.4.py
+++ Chapter 13/Ex13.4.py
@@ -4,10 +4,16 @@ def semi_copy_reverse(oldfile, newfile):
     infile.close()
 
     outfile = open(newfile, "w")
-    for line in xs:  # Take each line, split each word into a list, join back together without first number
+
+    # Take each line, split each word into a list, join back together without first number
+    for line in xs:  
         line_list = line.split(" ")  # Specify separator so \n is not removed in the splitting process
+
+        # Remove number in listing
         if line_list[0].isdigit():
-            line_list[0] = ""  # Remove number in listing
+            del line_list[0]
+
+        # Add numberless lines back together
         complete_line = " ".join(line_list)
         outfile.write(complete_line)
     outfile.close()
