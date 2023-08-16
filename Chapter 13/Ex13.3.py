7142c140e1aa84ec3bf1becfbf6729b2e728d161
datooff24 <132133001+datooff24@users.noreply.github.com>
2023-08-16 20:06:26 +0200

    Update Ex13.3.py

--- Chapter 13/Ex13.3.py
+++ Chapter 13/Ex13.3.py
@@ -1,16 +1,11 @@
 def semi_copy(oldfile, newfile):
+    # Idea: divide the file lines into a list, create a new string that is line number + file line
     infile = open(oldfile, "r")
     xs = infile.readlines()
     infile.close()
 
     outfile = open(newfile, "w")
-    for i in range(5):
-        if i >= len(xs):  # Prevents an error if the text file contains less than five lines
-            break
-        line_num = "{0:04d}".format(i+1) + " " + xs[i]  # Numbers the first five lines
-        outfile.write(line_num)
-    for i in xs[5:]:
-        outfile.write(i)  # Copies all the remaining lines
+    for i in range(len(xs)):
+        numbered_line = "{0:04d}".format(i+1) + " " + xs[i]  # Numbers the first five lines before the text
+        outfile.write(numbered_line)
     outfile.close()
-
-semi_copy("test2.txt", "test3.txt")
