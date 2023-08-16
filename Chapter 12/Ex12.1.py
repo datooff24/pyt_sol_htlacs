5cd0872e9a3722861f676d5d17e34b0fd98f2e62
datooff24 <132133001+datooff24@users.noreply.github.com>
2023-08-16 15:20:47 +0200

    Update Ex12.1.py

--- Chapter 12/Ex12.1.py
+++ Chapter 12/Ex12.1.py
@@ -1,11 +1,21 @@
+# 12.1.a
 import calendar
+cal = calendar.TextCalendar()
+cal.pryear(2012)
+
+# 12.1.b
 # firstweekday is set to zero i.e. monday by default.
 cal = calendar.TextCalendar(firstweekday=3)
 cal.pryear(2012)
+
+# 12.1.c
 # 0 corresponds to january, 1 to february etc.
 cal.prmonth(2023, 4)
 
+# 12.1.d
 d = calendar.LocaleTextCalendar(6, "norwegian")  # Norwegian works, Korean does not work
 d.pryear(2012)
 
-print(calendar.isleap(2000))  # Takes a year as argument, returns TRUE if the year is a leap year, FALSE otherwise
+# 12.1.e
+# Takes a year as argument, returns TRUE if the year is a leap year, FALSE otherwise
+print(calendar.isleap(2000))
