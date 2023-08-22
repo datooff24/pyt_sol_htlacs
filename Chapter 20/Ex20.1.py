8efa987a572be705fdb17a712d074ccfa2145789
datooff24 <132133001+datooff24@users.noreply.github.com>
2023-08-22 13:00:28 +0200

    Update Ex20.1.py

--- Chapter 20/Ex20.1.py
+++ Chapter 20/Ex20.1.py
@@ -12,3 +12,19 @@ def string_to_dic(string):
 
 
 string_to_dic("ThiS is String with Upper and lower case Letters")
+
+# ------ Alt sol ------
+
+def string_to_dic(strng):
+    dictionary = {}
+    strng = strng.lower()
+    for letter in strng:
+        if letter == " ":
+            continue
+        dictionary[letter] = dictionary.get(letter, 0) + 1
+    dict_list = list(dictionary.items())
+    dict_list.sort()
+    for key_value in dict_list:
+        print(key_value[0], key_value[1])
+
+string_to_dic("ThiS is String with Upper and lower case Letters")
